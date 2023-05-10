2a42391fdbeb2deeca81d1060f48af4d019f9a5c
imp2113sar <imp2113sar@gmail.com>
2023-05-10 14:59:03 +0400

    current-time api created

---
+++ index.js
@@ -0,0 +1,22 @@
+// Start the server
+const PORT = process.env.port || 8000;
+const express = require('express');
+const app = express();
+
+// Define the route for the API endpoint
+app.get('/current-time', (req, res) => {
+  // Get the current time
+  const currentTime = new Date();
+
+  // Create the JSON object with the current time
+  const responseData = {
+    time: currentTime.toISOString(),
+  };
+
+  // Send the JSON response
+  res.json(responseData);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
